40a1da87307f107af26783c1c8f4b99f5e87da10
eledezmacloudcall <30299213+eledezmacloudcall@users.noreply.github.com>
2017-07-20 10:49:03 -0500

    Inicio
    Los primero archivos arriba

---
+++ crm/application/views/admin/tables/customers_groups.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$aColumns = array('name');
+
+$sIndexColumn = "id";
+$sTable = 'tblcustomersgroups';
+
+$where = array();
+array_push($where, 'AND tblcustomersgroups.companyid = '. get_staff_company_id());
+$result = data_tables_init($aColumns,$sIndexColumn,$sTable,array(),$where,array('id'));
+$output = $result['output'];
+$rResult = $result['rResult'];
+
+foreach ( $rResult as $aRow )
+{
+    $row = array();
+    for ( $i=0 ; $i<count($aColumns) ; $i++ )
+    {
+        $_data = '<a href="#" data-toggle="modal" data-target="#customer_group_modal" data-id="'.$aRow['id'].'">'.$aRow[$aColumns[$i]].'</a>';
+
+        $row[] = $_data;
+    }
+    $options = icon_btn('#','pencil-square-o','btn-default',array('data-toggle'=>'modal','data-target'=>'#customer_group_modal','data-id'=>$aRow['id']));
+    $row[]  = $options .= icon_btn('clients/delete_group/'.$aRow['id'],'remove','btn-danger _delete');
+
+    $output['aaData'][] = $row;
+}
